a22ad91e93002b26590789bb5a7ba47964dec8cd
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-03-24 09:56:54 +0100

    Update student_scores_dictionary.py

--- student_scores_dictionary.py
+++ student_scores_dictionary.py
@@ -1,26 +1,21 @@
-student_scores = {
-  "Harry": 81,
-  "Ron": 78,
-  "Hermione": 99, 
-  "Draco": 74,
-  "Neville": 62,
-}
-# TODO-1: Create an empty dictionary called student_grades.
+student_scores = {}
+
+name = input("What is your name? ")
+score = int(input("What is your score? "))  # Convert score to integer
+
+student_scores[name] = score  # Add the entered name and score to the dictionary
 
 student_grades = {}
 
-# TODO-2: Write your code below to add the grades to student_grades.👇
 for student in student_scores:
-  score = student_scores[student]
-  #print(student)
- # print(student_scores[student])
-  if score > 90:
-    student_grades[student] = "Outstanding"
-  elif score > 80:
-    student_grades[student] = "Exceeds Expectations"
-  elif score > 70:
-    student_grades[student] = "Acceptable"
-  else:
-    student_grades[student] = "Fail"
+    score = student_scores[student]
+    if score > 90:
+        student_grades[student] = "Outstanding"
+    elif score > 80:
+        student_grades[student] = "Exceeds Expectations"
+    elif score > 70:
+        student_grades[student] = "Acceptable"
+    else:
+        student_grades[student] = "Fail"
 
 print(student_grades)
