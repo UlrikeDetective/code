48c21a433708b6c3526389ce028c5fbf8e7c67da
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-03-26 18:38:36 +0100

    Update student_scores_dictionary.py - Add subject

--- student_scores_dictionary.py
+++ student_scores_dictionary.py
@@ -5,6 +5,7 @@ while True:
     name = input("What is the student's name? (Enter 'quit' to stop): ")
     if name.lower() == 'quit':
         break  # Exit the loop if the user enters 'quit'
+    subject = input("What subject was the exam in? ")
     score = int(input("What is the student's score? "))
     student_scores[name] = score
 
@@ -20,7 +21,7 @@ for student, score in student_scores.items():
 
 print("Student Grades:")
 for student, grade in student_grades.items():
-    print(f"{student}: {grade}")
+    print(f"{student}: {subject} {grade}")
 
 # Next task:
 
