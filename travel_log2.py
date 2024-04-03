a95a9d07d165ec8aa294073eee4ae6c1f6344fbf
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-04-03 22:30:19 +0200

    Update travel_log2.py

--- travel_log2.py
+++ travel_log2.py
@@ -1,22 +1,19 @@
 travel_log = []
 
-def add_new_country(name, times_visited, cities_visited, highlights, favorite_things, meals):
+def add_new_country(name, times_visited, cities_visited, highlights):
     new_country = {}
     new_country['country'] = name
     new_country['visits'] = times_visited
     new_country['cities'] = cities_visited
     new_country['highlights'] = highlights
-    new_country['favorite_things'] = favorite_things
-    new_country['meals'] = meals
     travel_log.append(new_country)
 
 country = input("Add a country: ")
 visits = int(input("How many times have you visited that country? "))
-list_of_cities = eval(input("Name the cities you visited in that country? (Enter as a list): ")) # create list from formatted string
+list_of_cities = eval(input("Name the cities you visited in that country? (Enter as a list): ")) # create list from formatted string ['London', 'Bristol']
 highlights = input("Enter highlights: ")
-favorite_things = input("Enter favorite things: ")
-meals = input("Enter meals: ")
 
-add_new_country(country, visits, list_of_cities, highlights, favorite_things, meals)
+
+add_new_country(country, visits, list_of_cities, highlights)
 print(f"I've been to {travel_log[-1]['country']} {travel_log[-1]['visits']} times.")
 print(f"My favorite city was {travel_log[-1]['cities'][0]}.")
