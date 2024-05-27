57d69dc5f15dde45980bec7cd5b2ec20ceacc76e
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-05-27 20:50:59 +0200

    Update reminder.py

--- Python/reminder.py
+++ Python/reminder.py
@@ -1,16 +1,19 @@
 import schedule
 import time
 
-def post_to_twitter():
-    # Code to post to Twitter
-def post_to_facebook():
-    # Code to post to Facebook
-def post_to_instagram():
-    # Code to post to Instagram
+def post_to_DataSciense():
+
+def post_to_livingHealthy():
+
+def post_to_good_things():
+
+def post_to_journal():
+
 # Schedule posts to run at specific times
-schedule.every().day.at("08:00").do(post_to_twitter)
-schedule.every().day.at("12:00").do(post_to_facebook)
-schedule.every().day.at("16:00").do(post_to_instagram)
+schedule.every().day.at("21:40").do(post_to_DataScience)
+schedule.every().day.at("21:45").do(post_to_living_Healthy)
+schedule.every().day.at("21:50").do(post_to_good_things)
+schedule.every().day.at("21:55").do(post_to_journal)
 # Keep the script running to execute scheduled tasks
 while True:
     schedule.run_pending()
