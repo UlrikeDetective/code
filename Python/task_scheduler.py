4d6bf7eb43cd5ab3ef8ff12e8df0ea453e7e9658
[Ulrike Herold] <[herold.ulrike@gmail.com]>
2024-05-27 21:04:58 +0200

    bug fixed

--- Python/task_scheduler.py
+++ Python/task_scheduler.py
@@ -2,11 +2,13 @@ import schedule
 import time
 
 def remind_task(task):
-    # Code to remind about task
-# Schedule reminders for important tasks
+    print(f"Reminder: {task}")
+
+# Schedule tasks to run at specific times
 schedule.every().day.at("21:45").do(remind_task, task="daily habits report")
 schedule.every().monday.at("07:30").do(remind_task, task="Check tasks for the week")
 schedule.every().friday.at("16:00").do(remind_task, task="Start weekend")
+
 # Keep the script running to execute scheduled tasks
 while True:
     schedule.run_pending()
