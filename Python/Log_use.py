42655bf52c5055fbc6be4c6adb88bba5080b08f4
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-06-10 07:43:15 +0200

    Update Log_use.py

--- Python/Log_use.py
+++ Python/Log_use.py
@@ -1,20 +1,14 @@
 import csv
 from datetime import datetime
-from geopy.geocoders import Nominatim
+import geocoder
 
 def get_location():
-    # Create a geolocator object
-    geolocator = Nominatim(user_agent="geoapiExercises")
-
-    # Use your preferred method to get the coordinates (latitude and longitude)
-    # Here, we'll just use an example set of coordinates
-    # For example, you could use an IP address to location service to get the actual location
-    latitude = 40.7128
-    longitude = -74.0060
-
-    # Get the location information
-    location = geolocator.reverse((latitude, longitude), language='en')
-    return location.address
+    # Get the location based on the IP address
+    g = geocoder.ip('me')
+    if g.ok:
+        return g.latlng, g.city, g.state, g.country
+    else:
+        return None, None, None, None
 
 def record_entry():
     # Get current date and time
@@ -22,10 +16,10 @@ def record_entry():
     current_time = now.strftime("%Y-%m-%d %H:%M:%S")
 
     # Get location
-    location = get_location()
+    latlng, city, state, country = get_location()
 
     # Prepare the data to be written to CSV
-    data = [current_time, location]
+    data = [current_time, latlng, city, state, country]
 
     # Specify the CSV file name
     csv_file = 'recorded_data.csv'
