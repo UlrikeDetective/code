3a0ab22cddb62674776b59bf6e1e9da0a79ee55e
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-06-22 17:17:12 +0200

    Update index.js

--- web_development/jQuery/index.js
+++ web_development/jQuery/index.js
@@ -1,4 +1,13 @@
-$("h1").css("color", "blue");
+/* $(document).ready(function() {
+    $("h1").css("color", "blue");
+    $("button").css("color", "red"); 
+    $("button").css("background-color", "lightgreen");
+}); */
+
+/* $("h1").css("color", "blue");
 $("h1").css("font-size", "7rem");
 $("button").css("color", "red"); 
-$("button").css("background-color", "lightgreen");
+$("button").css("background-color", "lightgreen"); */
+
+$("h1").addClass("big-title margin-50");
+$("button");
