c783ff860da47eb11fad7f62d401865817e63d5b
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-06-29 10:46:53 +0200

    Create daily_habits_one_file.py

---
+++ Python/daily_habits/daily_habits_one_file.py
@@ -0,0 +1,278 @@
+import csv
+from datetime import datetime
+import geocoder
+import os
+
+def get_activity_input(prompt):
+    response = input(prompt).upper()
+    while response not in {'Y', 'N'}:
+        print("Invalid input. Please enter Y or N.")
+        response = input(prompt).upper()
+    return response
+
+def append_to_csv(file_path, fieldnames, data):
+    file_exists = os.path.isfile(file_path)
+    with open(file_path, mode='a', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        if not file_exists:
+            writer.writeheader()
+        writer.writerows(data)
+
+def check_and_get_unique_entry(file_path, date, log_type):
+    if os.path.isfile(file_path):
+        with open(file_path, mode='r', newline='') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                if row.get('Date') == date and row.get('log_type') == log_type:
+                    return None
+    return date
+
+def habit_tracker(filename, habit_questions, log_type):
+    activities = []
+    activity_id_counter = 1
+    total_activities = 0
+    today_date = datetime.now().strftime("%Y-%m-%d")
+    
+    if not check_and_get_unique_entry(filename, today_date, log_type):
+        print(f"Today's entries for {log_type} already exist. Exiting.")
+        return
+
+    while True:
+        name = input(f"Any activities for the {log_type} habit tracker today? (Enter 'quit' to stop): ")
+        if name.lower() == 'quit':
+            break
+
+        activity_data = {'Date': today_date, 'log_type': log_type}
+        for activity, prompt in habit_questions:
+            activity_data[activity] = get_activity_input(prompt)
+            if activity_data[activity] == 'Y':
+                if activity.endswith('_duration'):
+                    activity_data[activity] = input(f"How many minutes of {activity.split('_')[0]} did you do today? ")
+                elif activity == "Steps":
+                    activity_data['steps'] = input("How many steps did you do today? ")
+                elif activity == "other_sport":
+                    activity_data['other_sport'] = input("What other sport did you do today? ")
+                elif activity == "Book":
+                    activity_data['book'] = input("What book did you read today? ")
+                elif activity == "Listening":
+                    activity_data['listening'] = input("What audiobook did you listen to today? ")
+
+        activity_id = f"Habit{activity_id_counter:03}" if any(v == 'Y' for v in activity_data.values()) else None
+        total_tracker = sum(1 for v in activity_data.values() if v == 'Y')
+        total_activities += total_tracker
+
+        activity_data.update({
+            'activity_id': activity_id,
+            'daily_total': total_tracker,
+        })
+        activities.append(activity_data)
+        activity_id_counter += 1
+
+    if activities:
+        fieldnames = ['activity_id', 'Date', 'log_type', 'daily_total'] + [q[0] for q in habit_questions]
+        append_to_csv(filename, fieldnames, activities)
+
+def get_location():
+    # Get the location based on the IP address
+    g = geocoder.ip('me')
+    if g.ok:
+        return g.latlng, g.city, g.state, g.country
+    else:
+        return None, None, None, None
+
+def record_entry(filename):
+    # Get current date and time
+    now = datetime.now()
+    current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+
+    # Get location
+    latlng, city, state, country = get_location()
+
+    # Prepare the data to be written to CSV
+    data = [{
+        'Timestamp': current_time,
+        'Latitude and Longitude': latlng,
+        'City': city,
+        'State': state,
+        'Country': country,
+        'log_type': 'Location'
+    }]
+
+    # Specify the CSV file name
+    file_exists = os.path.isfile(filename)
+
+    # Write data to CSV file
+    fieldnames = ['Timestamp', 'Latitude and Longitude', 'City', 'State', 'Country', 'log_type']
+    append_to_csv(filename, fieldnames, data)
+
+    print(f"Recorded: {data}")
+
+def add_log_entry(filename):
+    # Define the header
+    header = ['Date', 'Data Science Programme', 'Focus', 'Focus Level', 'How Did It Go', 'log_type']
+    
+    # Define fixed choices
+    focus_levels = ['Beginner', 'Beginner/Intermediate', 'Intermediate', 'Intermediate/Advanced', 'Advanced', 'All level', 'None']
+    how_did_it_go_choices = {
+        '0': '0 = bad day - no Data Science',
+        '1': '1 = awful',
+        '2': '2 = maybe another day',
+        '3': '3 = still a lot to learn',
+        '4': '4 = fine',
+        '5': '5 = good',
+        '6': '6 = Knowledge is rising',
+        '7': '7 = fantastic',
+        '8': '8 = good day / holiday - sorry no Data Science'
+    }
+    
+    # Check if the file exists and is not empty
+    file_exists = os.path.isfile(filename) and os.path.getsize(filename) > 0
+
+    # Get current date
+    date = datetime.now().strftime('%Y-%m-%d')
+
+    # Prompt user for input
+    data_science_programme = input("Main used Data Science programme of the day: ")
+    focus = input("One thing you focused on: ")
+    
+    # Prompt for focus level
+    print("Choose the level of focus:")
+    for idx, level in enumerate(focus_levels, start=1):
+        print(f"{idx}. {level}")
+    focus_level_idx = int(input("Enter the number corresponding to the focus level: "))
+    focus_level = focus_levels[focus_level_idx - 1]
+    
+    # Prompt for how did it go
+    print("How did it go?")
+    for key, value in how_did_it_go_choices.items():
+        print(f"{key}. {value}")
+    how_did_it_go = input("Enter the number corresponding to your experience: ")
+    how_did_it_go = how_did_it_go_choices[how_did_it_go]
+    
+    # Prepare log entry
+    log_entry = [{
+        'Date': date,
+        'Data Science Programme': data_science_programme,
+        'Focus': focus,
+        'Focus Level': focus_level,
+        'How Did It Go': how_did_it_go,
+        'log_type': 'Data Science Log'
+    }]
+
+    # Write log entry to CSV file
+    append_to_csv(filename, header, log_entry)
+
+    print("Log entry added successfully!")
+
+def good_things_log(filename):
+    class GoodThingsLog:
+        def __init__(self, filename):
+            self.filename = filename
+            self.fieldnames = ['Date', 'First good thing', 'Second good thing', 'Third good thing', 'Honorary mentions', 'log_type']
+            self.check_and_create_file()
+
+        def check_and_create_file(self):
+            if not os.path.exists(self.filename):
+                with open(self.filename, 'w', newline='') as csvfile:
+                    writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames)
+                    writer.writeheader()
+
+        def add_log(self, first, second, third, honorary):
+            current_date = datetime.now().strftime('%Y-%m-%d')
+            if not check_and_get_unique_entry(self.filename, current_date, 'Good Things Log'):
+                print("Today's good things log already exists. Exiting.")
+                return
+            with open(self.filename, 'a', newline='') as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames)
+                writer.writerow({
+                    'Date': current_date,
+                    'First good thing': first,
+                    'Second good thing': second,
+                    'Third good thing': third,
+                    'Honorary mentions': honorary,
+                    'log_type': 'Good Things Log'
+                })
+
+        def display_logs(self):
+            with open(self.filename, 'r', newline='') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for idx, row in enumerate(reader, start=1):
+                    print(f"Good things Log #{idx}:")
+                    for key, value in row.items():
+                        print(f"{key}: {value}")
+                    print("\n")
+
+    good_things_log = GoodThingsLog(filename)
+
+    while True:
+        print("\nGood Things Log Menu:")
+        print("1. Add a new log")
+        print("2. Display all logs")
+        print("3. Exit")
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            first = input("Enter first good thing: ")
+            second = input("Enter second good thing: ")
+            third = input("Enter third good thing: ")
+            honorary = input("Enter honorary mentions: ")
+
+            good_things_log.add_log(first, second, third, honorary)
+            print("Log added successfully!")
+
+        elif choice == "2":
+            good_things_log.display_logs()
+
+        elif choice == "3":
+            print("Exiting Good Things Log.")
+            break
+
+        else:
+            print("Invalid choice. Please try again.")
+
+def main():
+    # Record location entry
+    record_entry('daily_tracker.csv')
+    
+    # Habit tracking
+    data_science_habit_questions = [
+        ("R", "Any R today? (Y/N): "),
+        ("SQL", "Any SQL today? (Y/N): "),
+        ("Python", "Any Python today? (Y/N): "),
+        ("Github", "Any Github today? (Y/N): "),
+        ("Kaggle", "Any Kaggle or competition today? (Y/N): "),
+        ("Terminal", "Any Terminal today? (Y/N): "),
+        ("Excel", "Any Excel today? (Y/N): "),
+        ("Adobe", "Any Adobe today? (Y/N): "),
+        ("website", "Any web development today? (Y/N): "),
+        ("others", "Any others today? (Y/N): "),
+        ("Career", "Did you work on your career today? (Y/N): "),
+        ("Networking", "Any networking today? (Y/N): "),
+        ("tech-learning", "Any tech learning today? (Y/N): "),
+        ("tech-reading", "Any tech reading today? (Y/N): "),
+        ("tech-listing", "Any tech listing today? (Y/N): ")
+    ]
+    habit_tracker('daily_tracker.csv', data_science_habit_questions, 'Data Science')
+    
+    living_healthy_habit_questions = [
+        ("Yoga", "Did you do yoga today? (Y/N): "),
+        ("Meditation", "Did you meditate today? (Y/N): "),
+        ("Steps", "Did you measure your steps today? (Y/N): "),
+        ("Biking_duration", "Did you ride a bike today? (Y/N): "),
+        ("other_sport", "Did you do any other sport today? (Y/N): "),
+        ("fruit", "Did you eat fruits today? (Y/N): "),
+        ("vegetables", "Did you eat vegetables today? (Y/N): "),
+        ("water", "Did you drink more than two liters of water today? (Y/N): "),
+        ("Book", "Did you read a book today? (Y/N): "),
+        ("Listening", "Did you listen to an audiobook or podcast today? (Y/N): ")
+    ]
+    habit_tracker('daily_tracker.csv', living_healthy_habit_questions, 'Living Healthy')
+
+    # Add log entry for data science
+    add_log_entry('daily_tracker.csv')
+
+    # Good things log
+    good_things_log('daily_tracker.csv')
+
+if __name__ == "__main__":
+    main()
