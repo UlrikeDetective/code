33410d205e56a9d9f46153ee898e87df8d88b230
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-07-05 16:25:11 +0200

    Update start_server.js

--- web_development/node/start_server.js
+++ web_development/node/start_server.js
@@ -9,8 +9,13 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+})
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
 });
 
 # 7. node file.js
+# stop running server: control + C
