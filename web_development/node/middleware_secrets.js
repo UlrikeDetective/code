c4f99988cc02defb96a3b2a919f107997ea9ead0
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-07-07 20:14:52 +0200

    Update middleware_secrets.js

--- web_development/node/middleware_secrets.js
+++ web_development/node/middleware_secrets.js
@@ -1,41 +1,31 @@
-# ILoveprogramming
-
 import express from "express";
 import bodyParser from "body-parser";
-import { dirname } from "path";
+import { dirname, join } from "path";
 import { fileURLToPath } from "url";
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
-var userIsAuthorised = false;
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
-function passwordCheck(req, res, next) {
-  const password = req.body["password"];
-  if (password === "ILoveProgramming") {
-    userIsAuthorised = true;
-  }
-  next();
-}
-app.use(passwordCheck);
+// Middleware to serve static files from the 'public' directory
+app.use(express.static(join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+  res.sendFile(join(__dirname, "public", "index.html"));
 });
 
 app.post("/check", (req, res) => {
-  if (userIsAuthorised) {
-    res.sendFile(__dirname + "/public/secret.html");
+  const password = req.body.password;
+  if (password === "ILoveProgramming") {
+    res.sendFile(join(__dirname, "public", "secret.html"));
   } else {
-    res.sendFile(__dirname + "/public/index.html");
-    //Alternatively res.redirect("/");
+    res.sendFile(join(__dirname, "public", "index.html"));
   }
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
-
