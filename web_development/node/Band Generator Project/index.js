fe3a5210912606d75d18941133db96ff73a95b81
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-07-12 21:27:23 +0200

    band generator finished

--- web_development/node/Band Generator Project/index.js
+++ web_development/node/Band Generator Project/index.js
@@ -4,28 +4,17 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
-//Step 3 - Make the styling show up.
-//Hint 1: CSS files are static files!
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Hint 2: The header and footer are partials.
-//Hint 3: Add the CSS link in header.ejs
-
-//Step 4 - Add a dynamic year to the footer.
-//Hint: Google to find out how to get the current year using JS.
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
 app.get("/", (req, res) => {
-  //Step 1 - Make the get route work and render the index.ejs file.
   res.render("index.ejs");
 });
 
 app.post("/submit", (req, res) => {
   const randomAdj = adj[Math.floor(Math.random() * adj.length)];
   const randomNoun = noun[Math.floor(Math.random() * noun.length)];
-  res.render("solution.ejs", {
+  res.render("index.ejs", {
     adjective: randomAdj,
     noun: randomNoun,
   });
