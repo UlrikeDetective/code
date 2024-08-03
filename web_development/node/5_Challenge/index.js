ea18d1ba1b87ac9d729852a8676deb4f2ef847c1
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-08-03 14:16:56 +0200

    Update index.js

--- web_development/node/5_Challenge/index.js
+++ web_development/node/5_Challenge/index.js
@@ -1,7 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Your other code...
+
 
 // Set EJS as the templating engine
 app.set('view engine', 'ejs');
