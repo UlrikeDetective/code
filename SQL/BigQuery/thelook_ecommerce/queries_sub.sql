2441e0aed7e3b37c837b56964a666c2b2e5aa7c7
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-08-26 18:41:27 +0200

    Update queries_sub.sql

--- SQL/BigQuery/thelook_ecommerce/queries_sub.sql
+++ SQL/BigQuery/thelook_ecommerce/queries_sub.sql
@@ -7,3 +7,14 @@ FROM
 `thelook_ecommerce.centers` AS centers) AS distance_to_closest_center
 FROM
 `thelook_ecommerce.customers` AS customers ;
+
+
+SELECT
+customers.id as customer_id,
+(
+SELECT
+MIN(ST_DISTANCE(centers.point_location, customers.point_location))/1000,
+FROM
+`thelook_ecommerce.centers` AS centers) AS distance_to_closest_center
+FROM
+`thelook_ecommerce.customers` AS customers ;
