7151ddb8a43ac69261e3a284632692bf6621485e
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-08-31 18:39:05 +0200

    SQL where and like

--- SQL/PostGre/food_test.sql
+++ SQL/PostGre/food_test.sql
@@ -8,3 +8,9 @@ SELECT * From public.world_food;
 
 SELECT country From public.world_food
 Where wheat_production > 20;
+
+SELECT * From public.world_food
+Where country like '%ina';
+
+SELECT * From public.world_food
+Where country like 'U' || '%';
