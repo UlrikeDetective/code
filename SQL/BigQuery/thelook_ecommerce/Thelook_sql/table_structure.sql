e87fc4360cfb3bb266a5440c0fc5bb16c9c16cdf
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-09-03 07:30:53 +0200

    TheLook modify

--- SQL/BigQuery/thelook_ecommerce/Thelook_sql/table_structure.sql
+++ SQL/BigQuery/thelook_ecommerce/Thelook_sql/table_structure.sql
@@ -8,12 +8,12 @@ CREATE TABLE distribution_centers (
     longitude FLOAT,
     PRIMARY KEY(id)
 );
+Drop table ga4_events;
 
 CREATE TABLE ga4_events (
     id INT,
     event_type VARCHAR(255),
     uri VARCHAR(255),
-    postal_code VARCHAR(20),
     traffic_source VARCHAR(255),
     browser VARCHAR(255),
     user_id INT,
@@ -23,7 +23,6 @@ CREATE TABLE ga4_events (
     created_at TIMESTAMP,
     sequence_number INT,
     city VARCHAR(255),
-    FOREIGN KEY(user_id) REFERENCES users.id,
     PRIMARY KEY(id)
 );
 
@@ -40,21 +39,18 @@ CREATE TABLE inventory_items (
     created_at TIMESTAMP,
     product_id INT,
     product_name VARCHAR(255),
-    FOREIGN KEY(product_id) REFERENCES products.id,
-    FOREIGN KEY(product_distribution_center_id) REFERENCES distribution_centers.id,
     PRIMARY KEY(id)
 );
 
 CREATE TABLE on_hand_inventory (
-    id SERIAL,
+    id INT AUTO_INCREMENT,
     on_hand_count INT,
     product_name_logistics VARCHAR(255),
     PRIMARY KEY(id)
 );
 
 CREATE TABLE order_items (
-    order_item_id,
-    id INT,
+    order_item_id INT AUTO_INCREMENT,
     order_id INT,
     product_id INT,
     user_id INT,
@@ -65,11 +61,9 @@ CREATE TABLE order_items (
     shipped_at TIMESTAMP,
     created_at TIMESTAMP,
     returned_at TIMESTAMP,
-    FOREIGN KEY(order.id) REFERENCES orders.order_id,
-    FOREIGN KEY(product.id) REFERENCES products.id,
-    FOREIGN KEY(inventory_item_id) REFERENCES inventory_items.id,
     PRIMARY KEY(order_item_id)
 );
+
 
 CREATE TABLE orders (
     order_id INT,
@@ -106,17 +100,19 @@ CREATE TABLE products (
 );
 
 CREATE TABLE shopping_cart (
-    cart_id SERIAL,
+    cart_id INT AUTO_INCREMENT,
     quantity INT,
     created_at TIMESTAMP,
     product_id INT,
     session_id VARCHAR(255),
-    FOREIGN KEY(product_id) REFERENCES products.id,
-    PRIMARY KEY(cart_id)
+    PRIMARY KEY(cart_id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
 );
 
 
+
 CREATE TABLE taxi_trips (
+	trip_id INT AUTO_INCREMENT,
     payment_type FLOAT,
     data_file_month INT,
     data_file_year INT,
