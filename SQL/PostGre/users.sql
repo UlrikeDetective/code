f1dbac4b74b59b9a305c602ac13e2298e91586c7
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-10-19 11:30:01 +0200

    Update users.sql

--- SQL/PostGre/users.sql
+++ SQL/PostGre/users.sql
@@ -4,6 +4,9 @@ name VARCHAR(15) UNIQUE NOT NULL,
 color VARCHAR(15)
 );
 
+INSERT INTO users (name, color)
+VALUES ('All', 'teal'), ('Wishlist', 'powderblue');
+
 DELETE FROM public.users
 
 Select * From public.users
