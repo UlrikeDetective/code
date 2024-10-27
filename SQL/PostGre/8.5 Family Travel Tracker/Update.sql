3263e3c6291b25d8dc1f26552929594607f599fd
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-10-27 18:47:15 +0100

    SQL Update V03

--- SQL/PostGre/8.5 Family Travel Tracker/Update.sql
+++ SQL/PostGre/8.5 Family Travel Tracker/Update.sql
@@ -6,6 +6,15 @@ Update public.contact_detail
 
 Select * From public.contact_detail;
 
+ALTER TABLE public.contact_detail
+ALTER COLUMN id SET NOT NULL;
+
+ALTER TABLE public.contact_detail
+ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY;
+
+ALTER TABLE public.contact_detail
+DROP CONSTRAINT contact_detail_id_fkey;
+
 INSERT INTO public.contact_detail (tel, address, email)
 VALUES 
     ('+4412457573', '543 Sunset', 'sunset@world.com'),
@@ -13,4 +22,17 @@ VALUES
     ('+4954745753', '63 Moon', 'moon@world.com'),
     ('+4865434563', '853 Galaxy', 'galaxy@world.com');
 
+INSERT INTO public.contact_detail (tel, address, email)
+VALUES 
+    ('+348737573', '43 New York', 'newyork@world.com'),
+    ('+2379557573', '192 Los Angeles', 'losangeles@world.com'),
+    ('+4955695753', '473 Seattle', 'seattle@world.com'),
+    ('+4654344563', '875 San Francisco', 'sanfrancisco@world.com');
+
 Select * From public.contact_detail;
+
+Delete from public.contact_detail
+	WHERE id != 1 OR id IS NULL;
+
+Delete from public.contact_detail
+	WHERE id = 1;
