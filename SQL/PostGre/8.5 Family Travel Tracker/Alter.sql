8208cfdfaaed34047df5e1ee694f94ab20e2c211
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-10-27 18:03:09 +0100

    SQL Alter V02

--- SQL/PostGre/8.5 Family Travel Tracker/Alter.sql
+++ SQL/PostGre/8.5 Family Travel Tracker/Alter.sql
@@ -1,2 +1,10 @@
 ALTER TABLE public.student
 RENAME TO users;
+
+ALTER TABLE public.users
+RENAME TO students;
+
+Select * FROM public.students;
+
+Alter table public.students
+	alter column first_name Type Varchar(20);
