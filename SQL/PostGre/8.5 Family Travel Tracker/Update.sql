64b53eafd1e937541a6b591b7c7c9afc0c06a3ab
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-10-27 18:32:01 +0100

    SQL Update Vo2

--- SQL/PostGre/8.5 Family Travel Tracker/Update.sql
+++ SQL/PostGre/8.5 Family Travel Tracker/Update.sql
@@ -5,3 +5,12 @@ Update public.contact_detail
 	where id = 1;
 
 Select * From public.contact_detail;
+
+INSERT INTO public.contact_detail (tel, address, email)
+VALUES 
+    ('+4412457573', '543 Sunset', 'sunset@world.com'),
+    ('+4379325573', '1072 Sunrise', 'sunrise@world.com'),
+    ('+4954745753', '63 Moon', 'moon@world.com'),
+    ('+4865434563', '853 Galaxy', 'galaxy@world.com');
+
+Select * From public.contact_detail;
