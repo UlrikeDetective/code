0f779b7fc7fccd97820e219e623756baebefe300
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-10-27 18:06:19 +0100

    SQL Alter V03

--- SQL/PostGre/8.5 Family Travel Tracker/Alter.sql
+++ SQL/PostGre/8.5 Family Travel Tracker/Alter.sql
@@ -8,3 +8,8 @@ Select * FROM public.students;
 
 Alter table public.students
 	alter column first_name Type Varchar(20);
+
+Alter Table contact_detail
+	add email TEXT;
+
+Select * FROM public.contact_detail;
