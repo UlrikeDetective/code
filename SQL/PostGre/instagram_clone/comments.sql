ea30d1370903bfe47fafa69de74450757fe02b59
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-12-01 10:40:13 +0100

    Create comments.sql

---
+++ SQL/PostGre/instagram_clone/comments.sql
@@ -0,0 +1,7 @@
+-- Insert sample comments
+INSERT INTO comments (user_id, post_id, content) VALUES
+(1, 2, 'Great post!'), 
+(2, 1, 'Amazing work!'), 
+(3, 1, 'Loving the vibe!'), 
+(1, 3, 'Looks awesome!'),
+(4, 2, 'Final comment.');
