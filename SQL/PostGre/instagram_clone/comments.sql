51695d77927f085c7dafde8173cced5c20054196
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-12-01 13:11:41 +0100

    Update comments.sql

--- SQL/PostGre/instagram_clone/comments.sql
+++ SQL/PostGre/instagram_clone/comments.sql
@@ -4,4 +4,8 @@ INSERT INTO comments (user_id, post_id, content) VALUES
 (2, 1, 'Amazing work!'), 
 (3, 1, 'Loving the vibe!'), 
 (1, 3, 'Looks awesome!'),
-(4, 2, 'Final comment.');
+(4, 2, 'Final comment.'),
+(9, 2, 'Wow, amazing shot!'), 
+(9, 3, 'Love this view!'), 
+(9, 4, 'Epic capture 🔥'),
+(10, 4, 'You inspire me!');
