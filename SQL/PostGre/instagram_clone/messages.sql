9a64a317954e3fc2de81b34f338f15ef7ad44da0
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-12-01 13:13:48 +0100

    Update messages.sql

--- SQL/PostGre/instagram_clone/messages.sql
+++ SQL/PostGre/instagram_clone/messages.sql
@@ -4,4 +4,8 @@ INSERT INTO messages (sender_id, receiver_id, content) VALUES
 (2, 1, 'I am good, thanks!'),
 (1, 3, 'What’s up?'),
 (3, 1, 'Not much, you?'),
-(4, 2, 'Catch you later!');
+(4, 2, 'Catch you later!'),
+(7, 2, 'Hey, check out my latest post!'), 
+(7, 1, 'Looks awesome!'),
+(9, 4, 'When are we catching up?'),
+(9, 1, 'See you soon!');
