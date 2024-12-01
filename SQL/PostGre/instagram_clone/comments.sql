75d23f4f5ae988053a62c973ebc9ed15aa6f31f9
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-12-01 13:36:23 +0100

    insta_clone V03

--- SQL/PostGre/instagram_clone/comments.sql
+++ SQL/PostGre/instagram_clone/comments.sql
@@ -9,3 +9,23 @@ INSERT INTO comments (user_id, post_id, content) VALUES
 (9, 3, 'Love this view!'), 
 (9, 4, 'Epic capture 🔥'),
 (10, 4, 'You inspire me!');
+
+-- List all comments on a specific post:
+SELECT u.username, c.content, c.created_at
+FROM comments c
+JOIN users u ON c.user_id = u.id
+WHERE c.post_id = 1;
+
+-- Add a comment
+INSERT INTO comments (user_id, post_id, content)
+VALUES (11, 1, 'This is such a cool photo!');
+
+-- Edit a comment
+UPDATE comments
+SET content = 'more of this'
+WHERE id = 1;
+
+-- Delete a comment
+DELETE FROM comments WHERE id = 1;
+
+
--- SQL/PostGre/instagram_clone/messages.sql
+++ SQL/PostGre/instagram_clone/messages.sql
@@ -9,3 +9,18 @@ INSERT INTO messages (sender_id, receiver_id, content) VALUES
 (7, 1, 'Looks awesome!'),
 (9, 4, 'When are we catching up?'),
 (9, 1, 'See you soon!');
+
+-- List all messages between two users:
+SELECT * 
+FROM messages
+WHERE (sender_id = 1 AND receiver_id = 2)
+   OR (sender_id = 2 AND receiver_id = 1)
+ORDER BY sent_at;
+
+-- send a message
+INSERT INTO messages (sender_id, receiver_id, content)
+VALUES (1, 2, 'Hello, how are you?');
+
+-- delete a message
+DELETE FROM messages WHERE id = 1;
+
