9397d15c28a9221d3f71b5d24529f3754b358a0e
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-12-01 10:40:54 +0100

    Create messages.sql

---
+++ SQL/PostGre/instagram_clone/messages.sql
@@ -0,0 +1,7 @@
+-- Insert sample messages
+INSERT INTO messages (sender_id, receiver_id, content) VALUES
+(1, 2, 'Hey, how are you?'),
+(2, 1, 'I am good, thanks!'),
+(1, 3, 'What’s up?'),
+(3, 1, 'Not much, you?'),
+(4, 2, 'Catch you later!');
