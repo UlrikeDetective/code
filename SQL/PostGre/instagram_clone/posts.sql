42d32153b8ce97176d230805a4fb2ddecf161185
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-12-01 13:07:30 +0100

    Update posts.sql

--- SQL/PostGre/instagram_clone/posts.sql
+++ SQL/PostGre/instagram_clone/posts.sql
@@ -5,6 +5,6 @@ INSERT INTO posts (user_id, image_url, caption) VALUES
 (3, 'https://example.com/image3.jpg', 'Great vibes! #life'),
 (4, 'https://example.com/image4.jpg', 'Final post for testing.'),
 (1, 'https://example.com/image1.jpg', 'Chilling in the Arctic 🐧'),
-(5, 'https://example.com/image2.jpg', 'Skyline dreams! 🌃'),
-(8, 'https://example.com/image3.jpg', 'Another peak conquered 🏔️'),
-(10, 'https://example.com/image20.jpg', 'This is just the beginning...');
+(9, 'https://example.com/image2.jpg', 'Skyline dreams! 🌃'),
+(3, 'https://example.com/image3.jpg', 'Another peak conquered 🏔️'),
+(3, 'https://example.com/image20.jpg', 'This is just the beginning...');
