71f8a15fc1407b3c6c7f16a5f7797f0000916d68
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2025-05-11 19:15:47 +0200

    start gsap V02

--- web_development/GSAP_animation/01start/index.js
+++ web_development/GSAP_animation/01start/index.js
@@ -1,1 +1,24 @@
-gsap.to(".box", { x: 200 })
+// use a class or ID
+gsap.to(".box", { x: 200 });
+
+// a complex CSS selector
+gsap.to("section > .box", { x: 200 });
+
+// a variable
+let box = document.querySelector(".box");
+gsap.to(box, { x: 200 })
+
+// or even an Array of elements
+let square = document.querySelector(".square");
+let circle = document.querySelector(".circle");
+                                      
+gsap.to([square, circle], { x: 200 })
+
+gsap.to(target, {
+  // this is the vars object
+  // it contains properties to animate
+  x: 200,
+  rotation: 360,
+  // and special properties
+  duration: 12
+})
